299cc964c13e8e0e987de292db7c342fdf462b6c
inas-mohammad <70091431+inas-mohammad@users.noreply.github.com>
2020-10-23 20:58:24 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
-const express = require('express');
-const router = require('./router');
+const express = require("express");
+const db = require('./db');
 require('dotenv').config();
+const todoModel = require("./schema");
+const router = require("./router");
+require("dotenv").confige();
+const db = require("./db");
+app.use(router);
 const app = express();
 app.use(express.json());
-app.use(router);
-const port = 3000;
+app.use (routrer);
+app.get("/todos", (req, res) => {});
+app.post("/create/todo", (req, res) => {});
+app.put("/update/todo", (req, res) => {});
+app.delete("/delete/todo", (req, res) => {});
 const bcrypt = require("bcrypt");
 console.log(process.env.DATABASE_HOST); 
 console.log(process.env.SECRET); 
+const { users, roles } = require('./models');
 
  password => my_secure_password
  salt => 21
@@ -41,6 +50,73 @@ const generateToken = () => {
 const authenticateToken = (token) => {
 
   const parsedToken = jwt.verify(token, SECRET);
+
+  const register = async (user) => {
+    console.log('USER: ', user);
+   
+    const savedUser = users.filter((u) => u.email === user.email);
+
+    if (savedUser.length === 0) {
+      
+      const newUser = user;
+     
+      newUser.id = 2;
+    
+      newUser.password = await bcrypt.hash(
+        user.password,
+        Number(process.env.SALT)
+      );
+      
+      users.push(newUser);
+     
+      return newUser;
+  
+    } else {
+      
+      return 'User already exists';
+    }
+    
+    
+  };
+  
+  const login = async (user) => {
+    const savedUser = users.filter((u) => u.email === user.email);
+  
+    if (savedUser.length === 0) {
+      return 'User Not Found please register';
+    } else {
+     
+      if (await bcrypt.compare(user.password, savedUser[0].password)) {
+        
+        const savedPermission = roles.filter((p) => p.id === savedUser[0].role_id);
+  
+        const payload = {
+          email: savedUser[0].email,
+          permissions: savedPermission[0].permissions,
+        };
+  
+        const options = {
+          expiresIn: process.env.TOKEN_EXPIRATION,
+        };
+        
+        return await jwt.sign(payload, process.env.SECRET, options);
+        
+      } else {
+        return 'Username or password not correct';
+      }
+    }
+    
+  };
+  
+  const getUsers = () => {
+    return users;
+  };
+  
+  module.exports = {
+    register,
+    login,
+    getUsers,
+  };
   
 
   app.post('/video', (req, res) => {
@@ -67,8 +143,7 @@ const authenticateToken = (token) => {
     
   });
 }
-const port =3000
+const port = 3000;
 app.listen(port, () => {
-    console.log(`W8 ON at http://localhost:${port}`);
-  });
-
+  console.log(`Example app listening at http://localhost:${port}`);
+});
