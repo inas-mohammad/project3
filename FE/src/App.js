62fe4c660834320a1717e0e02434e1b12db2fa17
inas-mohammad <inas.hameidan@gmail.com>
2020-11-01 19:35:50 +0200

    add front end

---
+++ FE/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div>hello world</div>;
+  }
+}
